877eac589b28ab46159f63cf10d672468f5affd3
KRITIK THAKUR <kritikthakur617@gmail.com>
2025-02-15 17:13:51 +0530

    Solution to Check LSB of a Number

---
+++ Check LSB of a Number@@67882a731b4392d8fc1577e8/code.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    int lsb=num&1;
+    if (lsb==1){
+        printf(Set);
+    }
+    else{
+        printf("Not Set");
+    }
+    return 0;
+}
